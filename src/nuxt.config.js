303aa2dfad944b43b70a5ef8adbb15e0c779af92
LBK_ikegami <ikegami@life-book.co.jp>
2025-11-29 14:09:30 +0900

    全体メンテナンス キレイキレイ

--- src/nuxt.config.js
+++ src/nuxt.config.js
@@ -1,18 +1,22 @@
 // `DEPLOY_ENV` が `GH_PAGES` の場合のみ `router.base = '/<repository-name>/'` を追加する
-const routerBase = process.env.DEPLOY_ENV === 'GH_PAGES' ? {
-  router: {
-    base: '/hieroglypher/'
-  }
-} : {}
+const routerBase =
+  process.env.DEPLOY_ENV === 'GH_PAGES'
+    ? {
+        router: {
+          base: '/hieroglypher/'
+        }
+      }
+    : {}
 
-const BASE_GH_URL = process.env.DEPLOY_ENV === 'GH_PAGES' ? '/hieroglypher/' : '/'
+const BASE_GH_URL =
+  process.env.DEPLOY_ENV === 'GH_PAGES' ? '/hieroglypher/' : '/'
 
 const BASE_TITLE = 'ヒエログリフ変換「ヒエログリファー」'
-const BASE_DESCRIPTION = '「ヒエログリファー」を使うと、ひらがな・カタカナ・アルファベットをリアルタイムでヒエログリフに変換することができます。'
+const BASE_DESCRIPTION =
+  '「ヒエログリファー」を使うと、ひらがな・カタカナ・アルファベットをリアルタイムでヒエログリフに変換することができます。'
 const BASE_URL = 'https://whike-chan.github.io/hieroglypher/'
 
 export default {
-  mode: 'universal',
   generate: {
     dir: '../docs'
   },
@@ -42,11 +46,36 @@ export default {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: BASE_GH_URL + 'favicon.ico' },
-      { rel: 'icon', type: 'image/png', size: '16x16', href: BASE_GH_URL + 'img/icon-16x16.png' },
-      { rel: 'icon', type: 'image/png', size: '32x32', href: BASE_GH_URL + 'img/icon-32x32.png' },
-      { rel: 'icon', type: 'image/png', size: '48x48', href: BASE_GH_URL + 'img/icon-48x48.png' },
-      { rel: 'icon', type: 'image/png', size: '72x72', href: BASE_GH_URL + 'img/icon-72x72.png' },
-      { rel: 'icon', type: 'image/png', size: '160x160', href: BASE_GH_URL + 'img/icon-160x160.png' }
+      {
+        rel: 'icon',
+        type: 'image/png',
+        size: '16x16',
+        href: BASE_GH_URL + 'img/icon-16x16.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        size: '32x32',
+        href: BASE_GH_URL + 'img/icon-32x32.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        size: '48x48',
+        href: BASE_GH_URL + 'img/icon-48x48.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        size: '72x72',
+        href: BASE_GH_URL + 'img/icon-72x72.png'
+      },
+      {
+        rel: 'icon',
+        type: 'image/png',
+        size: '160x160',
+        href: BASE_GH_URL + 'img/icon-160x160.png'
+      }
     ]
   },
   /*
